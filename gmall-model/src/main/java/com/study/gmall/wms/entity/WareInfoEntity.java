09bd25dc34421cd06061108987e931a6f698beb8
zxx <zhangxx@cheche365.com>
2020-11-13 16:08:37 +0800

    shop_wms_ware_info

---
+++ gmall-model/src/main/java/com/study/gmall/wms/entity/WareInfoEntity.java
@@ -0,0 +1,46 @@
+package com.study.gmall.wms.entity;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 
+ * 
+ * @author 张晓雄
+ * @email 824839090@qq.com
+ * @date 2020-11-13 15:59:10
+ */
+@ApiModel
+@Data
+@TableName("wms_ware_info")
+public class WareInfoEntity implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * id
+	 */
+	@TableId
+	@ApiModelProperty(name = "id",value = "id")
+	private Long id;
+	/**
+	 * 仓库名
+	 */
+	@ApiModelProperty(name = "name",value = "仓库名")
+	private String name;
+	/**
+	 * 仓库地址
+	 */
+	@ApiModelProperty(name = "address",value = "仓库地址")
+	private String address;
+	/**
+	 * 区域编码
+	 */
+	@ApiModelProperty(name = "areacode",value = "区域编码")
+	private String areacode;
+
+}
