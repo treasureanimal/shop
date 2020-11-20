d065a8136efd478478d7b83f2e2e985192ffb28b
zxx <zhangxx@cheche365.com>
2020-11-20 13:50:00 +0800

    保存spu相关属性

---
+++ gmall-model/src/main/java/com/study/gmall/sms/vo/SkuSaleVO.java
@@ -0,0 +1,25 @@
+package com.study.gmall.sms.vo;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+public class SkuSaleVO {
+
+    //积分营销相关字段
+    private BigDecimal growBounds;
+    private BigDecimal buyBounds;
+    private List<Integer> work;
+    //打折相关字段
+    private Integer fullCount;
+    private BigDecimal discount;
+    private Integer ladderAddOther;
+    //满减相关字段
+    private BigDecimal fullPrice;
+    private BigDecimal reducePrice;
+    private Integer fullAddOther;
+
+    private Long skuId;
+}
