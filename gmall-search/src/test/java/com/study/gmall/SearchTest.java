961fe1f5a0ce46dbfda44667f344ccd6f9efca28
zxx <zhangxx@cheche365.com>
2020-12-17 15:11:42 +0800

    准备elasticsearch测试案例导入Goods索引库数据

--- gmall-search/src/test/java/com/study/gmall/SearchTest.java
+++ gmall-search/src/test/java/com/study/gmall/SearchTest.java
@@ -1,20 +1,118 @@
 package com.study.gmall;
 
+import com.study.core.bean.PageVo;
+import com.study.core.bean.QueryCondition;
+import com.study.core.bean.Resp;
+import com.study.gmall.feign.GmallPmsClient;
+import com.study.gmall.feign.GmallWmsClient;
+import com.study.gmall.pms.entity.*;
+import com.study.gmall.repository.GoodsRepository;
 import com.study.gmall.search.pojo.Goods;
+import com.study.gmall.search.pojo.SearchAttr;
+import com.study.gmall.wms.entity.WareSkuEntity;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootTest
 public class SearchTest {
 
     @Autowired
     private ElasticsearchRestTemplate elasticsearchRestTemplate;
+    @Autowired
+    private GmallPmsClient pmsClient;
+    @Autowired
+    private GmallWmsClient wmsClient;
+    @Autowired
+    private GoodsRepository goodsRepository;
 
     @Test
-    public void contextLoads(){
+    public void contextLoads() {
         this.elasticsearchRestTemplate.createIndex(Goods.class);
         this.elasticsearchRestTemplate.putMapping(Goods.class);
+    }
+
+    @Test
+    void importData() {
+
+        Long pageNum = 1L;
+        long pageSize = 100L;
+
+        do {
+            QueryCondition queryCondition = new QueryCondition();
+            queryCondition.setPage(pageNum);
+            queryCondition.setLimit(pageSize);
+            Resp<List<SpuInfoEntity>> list = this.pmsClient.querySpuPage(queryCondition);
+            List<SpuInfoEntity> spus = list.getData();
+
+            //遍历spu查询sku
+            spus.forEach(SpuInfoEntity -> {
+                Resp<List<SkuInfoEntity>> listResp = this.pmsClient.querySkuBySpuId(SpuInfoEntity.getId());
+                List<SkuInfoEntity> data = listResp.getData();
+                if (!CollectionUtils.isEmpty(data)) {
+
+                    //把sku转化成goods对象
+                    List<Goods> goodsList = data.stream().map(skuInfoEntity -> {
+
+                        Goods goods = new Goods();
+
+                        //查询搜索属性及值
+                        Resp<List<ProductAttrValueEntity>> attrValueResp = this.pmsClient.querySearchAttrValueBySpuId(SpuInfoEntity.getId());
+                        List<ProductAttrValueEntity> attrValueEntities = attrValueResp.getData();
+                        if (!CollectionUtils.isEmpty(attrValueEntities)) {
+                            List<SearchAttr> searchAttrs = attrValueEntities.stream().map(productAttrValueEntity -> {
+                                SearchAttr searchAttr = new SearchAttr();
+                                searchAttr.setAttrId(productAttrValueEntity.getAttrId());
+                                searchAttr.setAttrName(productAttrValueEntity.getAttrName());
+                                searchAttr.setAttrValue(productAttrValueEntity.getAttrValue());
+                                return searchAttr;
+                            }).collect(Collectors.toList());
+                            goods.setAttrs(searchAttrs);
+                        }
+                        //查询品牌
+                        Resp<BrandEntity> brandEntityResp = this.pmsClient.queryBrandById(skuInfoEntity.getBrandId());
+                        BrandEntity brandEntity = brandEntityResp.getData();
+                        if (brandEntity != null) {
+                            goods.setBrandId(skuInfoEntity.getBrandId());
+                            goods.setBrandName(brandEntity.getName());
+                        }
+
+                        //查询分类
+                        Resp<CategoryEntity> categoryEntityResp = this.pmsClient.queryCategoryById(skuInfoEntity.getCatalogId());
+                        CategoryEntity categoryEntity = categoryEntityResp.getData();
+                        if (categoryEntity != null) {
+                            goods.setCategoryId(skuInfoEntity.getCatalogId());
+                            goods.setCategoryName(categoryEntity.getName());
+                        }
+                        goods.setCreateTime(SpuInfoEntity.getCreateTime());
+                        goods.setPic(skuInfoEntity.getSkuDefaultImg());
+                        goods.setPrice(skuInfoEntity.getPrice().doubleValue());
+                        goods.setSale(0L);
+                        goods.setSkuId(skuInfoEntity.getSkuId());
+
+                        //查询库存信息
+                        Resp<List<WareSkuEntity>> wareSkuBySkuId = this.wmsClient.queryWareSkuBySkuId(skuInfoEntity.getSkuId());
+                        List<WareSkuEntity> wareSkuEntities = wareSkuBySkuId.getData();
+                        if (!CollectionUtils.isEmpty(wareSkuEntities)) {
+                            boolean flag = wareSkuEntities.stream().anyMatch(wareSkuEntity -> wareSkuEntity.getStock() > 0);
+                            goods.setStore(flag);
+                        }
+                        goods.setTitle(skuInfoEntity.getSkuTitle());
+
+                        return goods;
+                    }).collect(Collectors.toList());
+                    this.goodsRepository.saveAll(goodsList);
+                }
+
+            });
+
+            pageSize = (long) spus.size();
+            pageNum++;
+        } while (pageSize == 100);
     }
 }
