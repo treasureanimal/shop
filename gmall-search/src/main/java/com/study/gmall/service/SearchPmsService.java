214ec8aad574f188c99303951d03ef4ac8b3b1e6
zxx <zhangxx@cheche365.com>
2020-12-22 19:30:02 +0800

    构建和解析搜索数据

--- gmall-search/src/main/java/com/study/gmall/service/SearchPmsService.java
+++ gmall-search/src/main/java/com/study/gmall/service/SearchPmsService.java
@@ -1,10 +1,11 @@
 package com.study.gmall.service;
 
 import com.study.gmall.search.pojo.SearchParamVO;
+import com.study.gmall.search.pojo.SearchResponseVO;
 
 import java.io.IOException;
 
 public interface SearchPmsService {
 
-    void search(SearchParamVO searchParamVO) throws IOException;
+    SearchResponseVO search(SearchParamVO searchParamVO) throws IOException;
 }
