5a59fb05dc3c5df626fdf8023e22484928259e70
zxx <zhangxx@cheche365.com>
2021-01-28 20:58:26 +0800

    校验锁定库存并防止重复提交

--- gmall-model/src/main/java/com/study/gmall/wms/vo/SkuLockVO.java
+++ gmall-model/src/main/java/com/study/gmall/wms/vo/SkuLockVO.java
@@ -7,4 +7,6 @@ public class SkuLockVO {
 
     private Long skuId; //需要锁定的商品
     private Integer count;  //锁多少件
+    private Boolean lock; //商品锁定状态
+    private Long wareSkuId;//锁定库存的id
 }
--- gmall-wms/src/main/java/com/study/gmall/service/impl/WareSkuServiceImpl.java
+++ gmall-wms/src/main/java/com/study/gmall/service/impl/WareSkuServiceImpl.java
@@ -10,13 +10,22 @@ import com.study.gmall.dao.WareSkuDao;
 import com.study.gmall.service.WareSkuService;
 import com.study.gmall.wms.entity.WareSkuEntity;
 import com.study.gmall.wms.vo.SkuLockVO;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service("wareSkuService")
 public class WareSkuServiceImpl extends ServiceImpl<WareSkuDao, WareSkuEntity> implements WareSkuService {
 
+    @Autowired
+    private RedissonClient redissonClient;
+    @Autowired
+    private WareSkuDao wareSkuDao;
     @Override
     public PageVo queryPage(QueryCondition params) {
         IPage<WareSkuEntity> page = this.page(
@@ -29,7 +38,42 @@ public class WareSkuServiceImpl extends ServiceImpl<WareSkuDao, WareSkuEntity> i
 
     @Override
     public String checkAndLockStore(List<SkuLockVO> skuLockVOS) {
+        if (CollectionUtils.isEmpty(skuLockVOS)) {
+            return "没有选中的商品";
+        }
+
+        skuLockVOS.forEach(skuLockVO -> {
+            locakStore(skuLockVO);
+        });
+        List<SkuLockVO> unlockStrore = skuLockVOS.stream().filter(skuLockVO -> skuLockVO.getLock() == false).collect(Collectors.toList());
+        if (!CollectionUtils.isEmpty(unlockStrore)) {
+            //解锁已锁定的商品
+            List<SkuLockVO> lockSku = skuLockVOS.stream().filter(SkuLockVO::getLock).collect(Collectors.toList());
+            lockSku.forEach(skuLockVO ->{
+                this.wareSkuDao.unlockStore(skuLockVO.getSkuId(),skuLockVO.getCount());
+            });
+            //商品不足锁定失败的商品
+            List<Long> unlockSku = unlockStrore.stream().map(SkuLockVO::getSkuId).collect(Collectors.toList());
+            return "下单失败，商品库存不足：" + unlockSku.toString();
+        }
         return null;
     }
 
+    private void locakStore(SkuLockVO skuLockVO){
+
+        RLock lock = this.redissonClient.getLock("stock:" + skuLockVO.getSkuId());
+        lock.lock();
+        //查询剩余库存够不够获取哪个仓库的商品充足
+        List<WareSkuEntity> wareSkuEntities = this.wareSkuDao.checkStore(skuLockVO.getSkuId(),skuLockVO.getCount());
+        if (!CollectionUtils.isEmpty(wareSkuEntities)) {
+            //锁定库存信息
+            Long id = wareSkuEntities.get(0).getId();
+            this.wareSkuDao.lockStore(id,skuLockVO.getCount());
+            skuLockVO.setLock(true);
+            skuLockVO.setSkuId(id);
+        }else{
+            skuLockVO.setLock(false);
+        }
+        lock.unlock();
+    }
 }
