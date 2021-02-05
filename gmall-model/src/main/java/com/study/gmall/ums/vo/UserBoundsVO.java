2bf93530446ff93aa94e108055add3f59463ee4e
zxx <zhangxx@cheche365.com>
2021-02-05 20:50:10 +0800

    下单成功减库存

---
+++ gmall-model/src/main/java/com/study/gmall/ums/vo/UserBoundsVO.java
@@ -0,0 +1,11 @@
+package com.study.gmall.ums.vo;
+
+import lombok.Data;
+
+@Data
+public class UserBoundsVO {
+
+    private Long memberId;
+    private Integer growth;
+    private Integer integration;
+}
--- gmall-wms/src/main/java/com/study/gmall/listener/WareListener.java
+++ gmall-wms/src/main/java/com/study/gmall/listener/WareListener.java
@@ -42,4 +42,23 @@ public class WareListener {
 
 
     }
+
+    /**
+     * 付完账减库存
+     *
+     * @param orderToken
+     */
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(value = "WMS-MINUS-QUEUE", durable = "true"),
+            exchange = @Exchange(value = "GMALL-ORDER-EXCHANGE", ignoreDeclarationExceptions = "true", type = ExchangeTypes.TOPIC),
+            key = {"stock.minus"}))
+    public void minusStoreListener(String orderToken) {
+        String lockJson = this.redisTemplate.opsForValue().get(KEY_PREFIX + orderToken);
+        List<SkuLockVO> skuLockVOS = JSON.parseArray(lockJson, SkuLockVO.class);
+        skuLockVOS.forEach(skuLockVO -> {
+            this.wareSkuDao.minusStore(skuLockVO.getWareSkuId(), skuLockVO.getCount());
+        });
+
+
+    }
 }
